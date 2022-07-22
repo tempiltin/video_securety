77178e623cfb9a101cce42d47eb8dd98c003ac39
__________ <91739351+tempiltin@users.noreply.github.com>
2022-07-22 10:49:48 +0500

    vIdeo_securety_programmer_uz_10_49_22_07_20022
    sdasdsad

---
+++ videojs/script.js
@@ -0,0 +1,36 @@
+// $(document).ready(function () {
+//     let data = {
+//         url: "./videoblocks-earth-spin-zoom-out-4k_rgz1pea1t__fb05f31002ea0be9bc5ee7b9732aad5f__P360.mp4",
+        
+//       }
+//     $('.vjs-big-play-button').on('click', function () {
+    
+
+//          console.log(data.url);
+
+
+//          $('video').innerHtml = `
+         
+//          <source src="${data.url}" type="/video/mp4">
+//          `
+         
+         
+        
+//         // $.ajax({
+//         //         type: 'GET',
+                
+//         //         url: "http://localhost:3000/api/v1",
+//         //         dataType: 'json',
+//         //         crossDomain: true,
+//         //         succes: function(object){
+//         //             $("h1").html(JSON.stringify(object));
+//         //         },
+//         //         error: function(object){
+//         //             $("h1").html(JSON.stringify(object));
+//         //         }
+//         //     })
+
+              
+//     })
+
+// });
